a60175c856d2b4f7e1d040a078d5f9904a6aee90
unknown <oa075855@gmail.com>
2022-09-02 03:31:13 +0500

    javascript

---
+++ app.js
@@ -0,0 +1,414 @@
+// Chapter # 1:
+
+// var a = ("Error! Please enter a valid password.");
+// alert(a);
+
+// var x = ( "Wellcome to JS Land... "); 
+// var y = ("Happy coding"!);
+// var z = x + y;
+// alert (z);
+
+
+// var a = ("Wellcome to JS Land...");
+// var b = ("Happy coding!")
+// var newLine = "\r\n"
+// var c = ("prevent this page from creating additional dialogs");
+// var d = b + c;
+// alert(a);
+// alert(d);
+
+// var a = ("Hello i can run JS through my web browser's console");
+// console.log(a);
+
+// chapter # 2; (VARIABLES FOR STRINGS )
+
+// var username =("OWAISALI853");
+// var myName = ("Syed M Owais Ali");
+// var message = ("Hello World");
+// alert(username);
+// alert(myName);
+// alert(message);
+
+// var email = ("My email is owaisali853@gmail.com");
+// alert (email);
+
+// var book = ("A smarter way to learn javascript");
+// alert(book);
+
+// var a = ("Yah! I can write HTML content through JavaScript");
+// document.write(a);
+
+// chapter # 3 VARIABLES FOR NUMBERS 
+
+// var age = ("I am 21 years old");
+// alert(age);
+
+
+// var birthYear = ("My birthyear is 2001");
+// document.write(birthYear);
+// var Visitorname = ("John Doe");
+// var Producttitle = (" T-shirt(s) XYZ Clothing store")
+// var  Quantity = ("ordered 5");
+// var c = Visitorname + Quantity + Producttitle;
+// document.write(c);
+
+// chapter # 4 VARIABLE NAMES: LEGAL & ILLEGAL 
+
+
+// var a = "“Rules for naming JS variables";
+// var myname = "owais ali";
+// var myName = "owais ali";
+// var my_name = "owais ali";
+// var _my_name = "owais ali";
+// var $my_1stname = "owais ";
+
+// var @myname = "Sadiq Shah";
+// var 123myName = "Sadiq Shah";
+// var my name = "sadiq shah";
+// var month&year = "saadiq shah";
+// var birth-year = "1990";
+// document.write(a);
+
+// chapter # 5 MATH EXPRESSIONS 
+
+// var a = 5;
+// var b = 10;
+// var c = a + b;
+// var d = "the sum of 5 and 10 is "
+// document.write(d , c)
+
+// var a = 5;
+// var b = 10;
+// var c = a * b;
+// var d = "the multiplication of 5 and 10 is "
+// document.write(d , c)
+
+// var a = 10;
+// var b = 3;
+// var c = a % b;
+// var d = "the modulus of 10 and 3 is "
+// document.write(d , c)
+
+// var a = 10;
+// var b = 3;
+// var c = a - b;
+// var d = "the subtraction of 10 and 3 is "
+// document.write(d , c)
+
+// var a = 10;
+// var b = 3;
+// var c = a / b;
+// var d = "the division of 10 and 3 is "
+// document.write(d , c)
+
+// var a = (5);
+// var b = ("Initial value is : ");
+// document.write(b , a);
+
+// var a = (5);
+// var b = ("value after increment : ");
+// var c = ++a ;
+// document.write(b , c);
+
+
+
+// var a = (5);
+// var b = ("value after addition : ");
+// var c = ++a + 7 ;
+// document.write(b , c);
+
+// var a = (13);
+// var b = ("value after decrement : ");
+// var c = --a  ;
+// document.write(b , c);
+
+// var a = (12);
+// var b = ("The remainder is : ");
+// var c = 3   ;
+// var d = a % c ;
+// document.write(b , d);
+
+// var ticketprice = (600);
+// var totaltickets = 5 ;
+// var totalcost = ticketprice * totaltickets ;
+// var a = ("Total cost of buying 5 tickets to a movie is ")
+// var b = ("PKR") ;
+// document.write(a ,  totalcost , b);
+
+// var table = 5;
+// var length = 10;
+// var i = 1;
+// document.write("Multiplication table: "+ table);
+// for(i=1; i <= length; i++)
+//  document.write("<br>"+i+" * "+table+" = " +(i * table));
+
+// var a = ("Shopping Cart");
+// var priceofitems1 = ("price of items 1 is 650")
+// var quantityofitems1 = ("price of items 1 is 3");
+// var priceofitem2 = ("price of items 2 is 100");
+// var quantityofitems2 = ("price of items 2 is 7");
+// var dc = ("Shipping Charges 100");
+// var totalcost = ( "Total cost of your order is 2750");
+// document.write(a , "<br>" , priceofitems1 , "<br>" , quantityofitems1 ,  "<br>" , priceofitem2 , "<br>" , quantityofitems2 , "<br>" , dc , "<br>" , totalcost );
+
+// var totalmarks = 100 ;
+// var obtainedmarks = 50 ;
+// var a = "Total marks:" ;
+// var b = "Obtained marks:";
+// var c = "Percentage:";
+// document.write(a , totalmarks , "<br>" , b , obtainedmarks , "<br>" , c  ,);
+
+// var heading = "Currency in PKR"
+// var usdollartopkr = 104.80;
+// var usdollarquantity = 10 ;
+// var saudiriyaltopkr = 28 ;
+// var saudiriyalquantity = 25 ;
+// var a = "Total Currency in PKR: "
+// var totalcurrencttopkr = (usdollartopkr * usdollarquantity) + (saudiriyaltopkr * saudiriyalquantity )    ;
+// document.write(heading , "<br>" , a , totalcurrencttopkr);
+
+// var a = 5 ;
+// var b = 10 
+// var c = 2 ;
+// var d = (a * b) / c ;
+// document.write(d);
+
+// var a = "Age Calculator:" ;
+// var b = "Birth Year:";
+// var c = "CurrentYear:" ;
+// var d = "Your age : ";
+// var birthyear = 1972 ;
+// var currentyear = 2022 ;
+// var age = currentyear - birthyear ;
+// document.write(a , "<br>" , b , birthyear , "<br>" , c , currentyear , "<br>" , d , age);
+
+// chapter 5 to 9 MATH EXPRESSIONS 
+
+// var a = 10;
+// var b = ++a;
+// var b = a++;
+// var b = --a;
+// var b = a--;
+// document.write(b);
+
+// var a = 2;
+// var b = 1;
+// var c = --a - --b + ++b + b--;
+// document.write(c);
+
+// chapter 9 - 11 USER INPUT & CONDITIONAL STATEMENT
+
+// var city = prompt("Enter your city")
+// if (city == "karachi"){
+//     alert("Welcome to city of lights")
+// }
+
+// var gender = prompt("Enter your gender")
+// if (gender == "male"){
+//     alert("Good Morning sir")
+// }
+// if (gender == "female"){
+//     alert("good morning mam")
+    
+// }
+
+// var signal = prompt("Enter signal color")
+// if (signal == "red"){
+//     alert("Must Stop")
+// }
+// if (signal == "yellow"){
+//     alert("Ready to move")
+    
+// }
+// if (signal == "green"){
+//     alert("move now")
+    
+// }
+
+// var fuel = +prompt("enter your car's fuel")
+// if (fuel == 0.2) {
+//     alert("Please refill the fuel in your car")
+// }
+
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+//  var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+
+//  var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+//  if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False"); 
+// }
+
+// if("car" < "cat"){
+//     alert("car is smaller than cat");
+//     }
+
+// var num = prompt("guess the secret number")
+// if (num == 7){
+//     alert("Bingo! Correct answer")
+// }
+// if (num == 8){
+//     alert("Close enough to the correct answer")
+// }
+
+// var divisible = +prompt("Enter your value")
+// if (divisible == 6 , 9 , 12 , 15 , 18 ,21 ){
+//     alert("yes its divisible by 3")
+// }
+    
+// var num = +prompt ("Enter Your value")
+// if (num == 2 , 4 , 8 , 10 , 12 , 14 , 16 ,18 ,20){
+//     alert("even number")
+// }
+// if (num ===1 , 3 , 5 , 7 , 9 , 11 , 13 , 15 ,17 , 19 , 21){
+//     alert("odd number")
+// }
+
+// var T = +prompt("Enter temperature and check how's the weather")
+// if (T > "4O"){
+//     alert("“It is too hot outside")
+// }
+// if(T > 30 ){
+//     alert("The Weather today is Normal")
+// }
+// if(T > 20 ){
+//     alert("Today’s Weather is cool")
+// }
+// if (T > 10){
+//     alert("OMG! Today’s weather is so Cool")
+// }
+
+// chapter 12 to 13 ; IF…ELSE & ELSE IF STATEMENT, TESTING SET OF CONDITIONS 
+
+
+// var num = prompt("Enter a number")
+// if (num == "0"){
+//     alert("number is zero")
+// }
+// if (num > "0"){
+//     alert("number is positive")
+// }
+// if (num < "0"){
+//     alert("number is negative")
+// }
+
+// function isVowel(argument){
+
+// 	var text;
+// 	var argument = argument.toLowerCase();
+
+//     var vowels = (['a', 'e', 'i', 'o', 'u']);
+
+//     for (var i = 0; i <= vowels.length; i++){
+//         if (argument != vowels[i]) {
+//             continue;
+//         }
+//         return true;
+//     }
+//     return false;
+// }
+
+// var char = "A";
+
+// if (isVowel(char)) {
+// 	alert(char + " is a vowel");
+// } else {
+// 	alert(char + " is not a vowel");
+// }
+
+// var pass = prompt("Enter your password")
+// if (pass == "owaisali" || "owaisali123"){
+//     alert("Correct! The password you entered matches the original password")
+// }
+// else if (pass !== "owaisali" || "owaisali123") {
+//     alert(" Please enter your correct password")
+// }
+
+// var pass = prompt("Enter your password")
+// if (pass == "123"){
+//     if (pass = "456"){
+//         alert("Correct! The password you entered matches the original password")
+//     }
+
+// }
+// else {
+//     alert("Please enter your correct password")
+// }
+
+// arrays chapter 14 to 16
+
+// var arr = [];
+
+// var objectA = {}
+
+// var arr = ["owais"];
+
+// var arr = [123 , 456 , 789];
+
+// var arr = [123 , "owais" , "helloworld"];
+
+// var arr = ["SSC" , "HSC" , "BCS" , "BS" ,"BCOM" ,"MS", "M.Phil","PhD"];
+// document.write(arr)
+
+// var arr = ["owais" , "ali" , "irtiza"]
+// var arr2 = [320 , 230 , 480]
+
+// var arr = ["blue" , "red " , "white" , "black"]
+// arr.unshift("brown")
+// arr.push("green")
+// arr.unshift("purple","yellow")
+// arr.shift()
+// arr.pop()
+// arr.splice(2,0 , "pink")
+// arr.slice(2,4)
+// document.write(arr);
+
+// var score = [320,230,480,120]
+// var oscore =[120,230,320,480]
+
+// var city = ["karachi " , "lahore", "islamabad","quetta","peshawar"]
+// console.log(city.slice(2,4))
+
+// var arr = ["This","is","my","cat"]
+// var arr = arr.join(" ")
+// document.write(arr)
+
+
+// array and loop
+
+// var num = [1,2,3,4,5,6,7,8,9,10]
+// document.write(num)
+
+// var arr = ["cake" , "apple pie" , "cookie", "chips" , "patties"]
+// var arr = prompt(" ");
+// if (arr == [0,1,2,3,4,]){
+//     alert("availabe")
+// }
+
